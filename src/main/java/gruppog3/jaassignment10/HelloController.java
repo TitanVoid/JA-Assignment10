fd3f2775921c2ebc5f8ffd12e75826e29aaddbcb
Paolo Alfe <paolovoid@gmail.com>
2026-04-28 14:23:26 +0200

    Initial commit

---
+++ src/main/java/gruppog3/jaassignment10/HelloController.java
@@ -0,0 +1,14 @@
+package gruppog3.jaassignment10;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module gruppog3.jaassignment10 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens gruppog3.jaassignment10 to javafx.fxml;
+    exports gruppog3.jaassignment10;
+}
