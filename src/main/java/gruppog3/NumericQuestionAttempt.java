58a8fcf61a68fe4cff4fa0e35b2a2f8d7921d3ea
Francesco Altieri <fra04alt@gmail.com>
2026-04-29 21:25:56 +0200

    NumericQuestionAttempt

---
+++ src/main/java/gruppog3/NumericQuestionAttempt.java
@@ -0,0 +1,38 @@
+package gruppog3;
+
+public class NumericQuestionAttempt {
+
+        private final NumericQuestion question;
+        private final int givenAnswer;
+
+        public NumericQuestionAttempt(NumericQuestion question, int givenAnswer) {
+            this.question = question;
+            this.givenAnswer = givenAnswer;
+        }
+
+        public NumericQuestion getQuestion() {
+            return question;
+        }
+
+        public int getGivenAnswer() {
+            return givenAnswer;
+        }
+
+        public boolean isCorrect() {
+            
+            return givenAnswer == question.getResult();
+        }
+
+        public String getResult() {
+            return isCorrect() ? "Corretto" : "Sbagliato";
+        }
+
+        @Override
+        public String toString() {
+            return "Domanda: " + question +
+                    " | Risposta data: " + givenAnswer +
+                    " | Esito: " + getResult();
+        }
+
+
+}
